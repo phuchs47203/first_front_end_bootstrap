8f9dd391a20fdb9bff1492514c1663d47407fa18
phuchs47203 <21522479@gm.uit.edu.vn>
2023-10-09 15:01:25 +0700

    edit background img

--- src/Container/Overview/Overview.jsx
+++ src/Container/Overview/Overview.jsx
@@ -4,12 +4,12 @@ import { imagess } from '../../constants';
 const Overview = () => {
     return (
         <div className='overview'>
-            <div className='overview-img'>
-                <img src={imagess.overview2} alt="" />
-            </div>
             <div className='overview-container'>
                 <h1>Creating safe AGI that benefits all of humanity</h1>
                 <button>Learn about OpenAI</button>
+            </div>
+            <div className='overview-post'>
+
             </div>
         </div>
     )
